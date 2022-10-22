6a1fbcf3c67fba937b4ff911f59eae7befe79941
akshi <neni.akki@gmail.com>
2022-10-22 01:45:00 -0400

    Added adjustable speed and jump

--- 2DGameTesting/Assets/Scripts/PlayerMovement.cs
+++ 2DGameTesting/Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Variable to adjust speed of player
+    //SerializeField - makes it adjustable from inside Unity
+    [SerializeField] private float speed; 
     //Reference to the player's rigidbody - 2d physics of player
     private Rigidbody2D body; 
 
@@ -26,6 +29,15 @@ public class PlayerMovement : MonoBehaviour
         //assigns velocity - speed in 2 directions
         //1st coordinate - left-right
         //2nd coordinate - no changing velocity in y 
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"), body.velocity.y);
+        //multiplying input from x-axis by speed to make player go faster
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        //adding jumping movement
+        //checking for space key button pressed
+        if(Input.GetKey(KeyCode.Space)){
+            //similar code to left-right, but in y direction
+            //can use a diff number for speed to tweak jump
+            body.velocity = new Vector2(body.velocity.x, speed);
+        }
     }
 }
